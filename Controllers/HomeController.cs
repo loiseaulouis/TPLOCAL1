890c65ac60f7fc08447ac5b695a453cf020ab770
loiseaulouis <80586501+loiseaulouis@users.noreply.github.com>
2025-02-04 15:32:42 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using TPLOCAL1.Models;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //methode "naturally" call by router
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //retourn to the Index view (see routing in Program.cs)
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "OpinionList":
+                        //TODO : code reading of the xml files provide
+                        List<Opinion> opinionList = new OpinionList().GetAvis("XlmFile/DataAvis.xml");
+                        return View(id, opinionList);
+                    case "Form":
+                        //TODO : call the Form view with data model empty
+                        return View(id);
+                    default:
+                        //retourn to the Index view (see routing in Program.cs)
+                        return View();
+                }
+            }
+        }
+
+
+        //methode to send datas from form to validation page
+        [HttpPost]
+        public ActionResult ValidationFormulaire(FormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["Message"] = "Le formulaire est invalide, veuillez réessayer.";
+
+                return View("~/Views/Shared/Form.cshtml", model);
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+    }
+}
---
+++ Models/Form.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+namespace TPLOCAL1.Models
+{
+    public class FormModel
+    {
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Adresse { get; set; }
+        [RegularExpression("[0-9]{5}")]
+        [Required]
+        public string CodePostal { get; set; }
+        [Required]
+        public string Ville { get; set; }
+        [RegularExpression("^([\\w]+)@([\\w]+).([\\w]+)$")]
+        [Required]
+        public string AdresseEmail { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly? DateDeb { get; set; }
+       
+        [Required]
+        public string Formation { get; set; }
+        public string? AvisCBL { get; set; }
+        public string? AvisDotNET { get; set; }
+    }
+}
